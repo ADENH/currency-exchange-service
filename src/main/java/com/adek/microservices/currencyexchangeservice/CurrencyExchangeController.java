10ef870889d3e67db2af7ad95e685d73d755f8a9
Ade Nur Hidayat <adenurhidayat101294@gmail.com>
2018-11-24 12:48:45 +0700

    retrieve from database

--- src/main/java/com/adek/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/adek/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,6 @@
 package com.adek.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
+import java.math.BigDecimal;import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -14,10 +14,14 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environtment;
 	
+	@Autowired
+	ExchangeValueRepository repository;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
-		ExchangeValue exchangeValue = 
-				new ExchangeValue(1L,from,to,BigDecimal.valueOf(10));
+		
+		ExchangeValue exchangeValue= repository.findByFromAndTo(from, to);
+		//ExchangeValue exchangeValue = new ExchangeValue(1L,from,to,BigDecimal.valueOf(10));
 		//set dinamic port
 		exchangeValue.setPort(
 				Integer.parseInt(environtment.getProperty("local.server.port")));
